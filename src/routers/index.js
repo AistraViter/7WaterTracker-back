a15e77bb0cf1c5156883e7b6abe2214e2e2fdaa5
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-10-16 16:22:42 +0300

    Додано до основного роутера  router.use('/auth', authRouter);

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import usersRouter from './users.js';
+import authRouter from './auth.js';
+
 
 const router = Router();
 
 router.use('/users', usersRouter);
+router.use('/auth', authRouter);
+
 
 export default router;
