8c6d9ca1ccfe601a77de72a61515c8bf9fa7022a
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-10-18 01:29:42 +0300

    Register fixed

--- src/middlewares/validateRegistrationData.js
+++ src/middlewares/validateRegistrationData.js
@@ -1,16 +1,16 @@
-import  createHttpError from 'http-errors';
+// import  createHttpError from 'http-errors';
 
-export const validateRegistrationData = (schema) = async(req, res, next) => {
-  const { error } = schema.validate(req.body, {
-    abortEarly: false,
-  });
+// export const validateRegistrationData = (schema) = async(req, res, next) => {
+//   const { error } = schema.validate(req.body, {
+//     abortEarly: false,
+//   });
 
-  if (error) {
-    const errorMessage = error.details
-      .map((detail) => detail.message)
-      .join(', ');
-    return next(createHttpError(400, errorMessage));
-  }
+//   if (error) {
+//     const errorMessage = error.details
+//       .map((detail) => detail.message)
+//       .join(', ');
+//     return next(createHttpError(400, errorMessage));
+//   }
 
-  next();
-};
+//   next();
+// };
