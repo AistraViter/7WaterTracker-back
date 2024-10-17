8bef10252465fde0a2b9041261e7fd365e789e01
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-10-18 01:18:43 +0300

    waterMonth router

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,11 +3,16 @@ import { Router } from 'express';
 import { usersRouter } from './users.js';
 import { authRouter } from './auth.js';
 import { waterNotesRouter } from './waterNotes.js';
+import { waterMonthRouter } from './waterMonth.js';
+
 
 const router = Router();
 // Всі маршрути пишемо тут
 router.use('/auth', authRouter);
 
-router.use('/water', waterNotesRouter, usersRouter); // об'єднала маршрути
+router.use('/water', waterNotesRouter, waterMonthRouter); 
+
+router.use('/', usersRouter); 
+
 
 export default router;
