e940236d28bfe4d5647be7fa05f0e228c8033403
Galyna Ch <kirsikka.gala@gmail.com>
2024-10-18 01:42:17 +0300

    corrected import of changed model

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { validateBody } from '../middlewares/validateBody.js';
-// import { UsersCollection } from '../db/models/users.js';
-import { User } from '../db/models/user.js';
+import { UsersCollection } from '../db/models/user.js';
+// import { User } from '../db/models/user.js';
 import { isValidId } from '../middlewares/isValidId.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { updateDailyNormController } from '../controllers/users.js';
@@ -15,5 +15,5 @@ usersRouter.put(
   isValidId('userId'),
   authenticate,
   ctrlWrapper(updateDailyNormController),
-  validateBody(User),
+  validateBody(UsersCollection),
 );
