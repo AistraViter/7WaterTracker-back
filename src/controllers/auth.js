c133868effdc8cfe1f1a08547a5508d9f1ccb7e6
Yelyzaveta756 <selanta19@gmail.com>
2024-10-17 23:42:43 +0300

    add reset-pwd

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,4 @@
-import { registrationUser } from '../services/auth.js';
+import { registrationUser, sendResetUser, resetPassword } from '../services/auth.js';
 import { JWT_EXPIRES_IN } from '../constants/index.js';
 import { JWT_SECRET } from '../constants/index.js';
 import { REFRESH_TOKEN_EXPIRES_IN } from '../constants/index.js';
@@ -162,5 +162,26 @@ export async function logoutUserController(req, res, next) {
 //     next(error); // Передаем ошибку в middleware для обработки
 //   }
 // }
+
+export const sendResetEmailController = async (req, res) => {
+  await sendResetUser(req.body.email);
+
+  res.status(200).json({
+    status: 200,
+    message: "Reset password email has been successfully sent.",
+    data: {}
+});
+};
+
+export const resetPasswordController = async (req, res) => {
+  await resetPassword(req.body);
+
+  res.status(200).json({
+    status: 200,
+    message: "Password has been successfully reset.",
+    data: {}
+});
+};
+
 
 
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,18 +1,26 @@
 import { Router } from 'express';
 import { validateRegistrationData } from '../middlewares/validateRegistrationData.js';
-import { registrationUserController,loginUserController,logoutUserController} from '../controllers/auth.js';
+import {
+  registrationUserController,
+  loginUserController,
+  logoutUserController,
+  sendResetEmailController,
+  resetPasswordController} from '../controllers/auth.js';
 import { validateBody } from '../middlewares/validateBody.js'; // Middleware для валидации тела запроса
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import {
   userLoginSchema,
-} from '../validation/userValidation.js'; // Схемы валидации
+  requestResetEmailSchema,
+  resetPasswordSchema
+} from '../validation/userValidation.js';
+
 
 
 export const authRouter = Router();
 
 authRouter.post(
   '/register',
-  validateRegistrationData, // Це треба переробити в стилі як зроблено логін. 
+  validateRegistrationData, // Це треба переробити в стилі як зроблено логін.
   registrationUserController,
 );
 
@@ -26,3 +34,8 @@ authRouter.post(
 
 // Маршрут для логаута пользователя
 authRouter.post('/logout', ctrlWrapper(logoutUserController)); // Добавляем контроллер для логаута
+
+authRouter.post('/send-reset-email', ctrlWrapper(sendResetEmailController), validateBody(requestResetEmailSchema));
+
+authRouter.post('/reset-password', validateBody(resetPasswordSchema), ctrlWrapper(resetPasswordController));
+
