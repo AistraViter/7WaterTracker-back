742d9454e882dfa1e7d54fd6930e8c181724cbe6
Yelyzaveta756 <selanta19@gmail.com>
2024-10-17 22:12:00 +0300

    +

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,10 @@
+import path from 'node:path';
+
 export const JWT_SECRET = process.env.JWT_SECRET || 'secretKey';
 export const JWT_EXPIRES_IN = '15m';
 export const REFRESH_TOKEN_EXPIRES_IN = '30d';
 
 export const accessTokenValidUntil = new Date(Date.now() + 15 * 60 * 1000); // Токен на 15 минут
 export const refreshTokenValidUntil = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000); // Токен на 30 дней
+
+export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
