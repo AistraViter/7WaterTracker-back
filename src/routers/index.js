9a240f5532a8862f021c0442082c99f6b3ba09b1
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-10-17 15:45:35 +0300

    Маршрути перенесено на routers/index.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,12 +1,11 @@
 import { Router } from 'express';
-import {usersRouter} from './users.js';
-import {authRouter} from './auth.js';
-
+import { usersRouter } from './users.js';
+import { authRouter } from './auth.js';
+import { waterNotesRouter } from './waterNotes.js';
 
 const router = Router();
-
-router.use('/', usersRouter);
+// Всі маршрути пишемо тут
 router.use('/auth', authRouter);
-
+router.use('/water-options', waterNotesRouter, usersRouter); // об'єднала маршрути
 
 export default router;
