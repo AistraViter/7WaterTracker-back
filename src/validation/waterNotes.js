df25ec5320e3e337df8c5aee5ebcaa57ba664aac
Vita1208 <111512978+Vita1208@users.noreply.github.com>
2024-10-18 12:21:42 +0300

    add waterMonth

--- src/validation/waterNotes.js
+++ src/validation/waterNotes.js
@@ -9,3 +9,12 @@ export const updateWaterNotesSchema = Joi.object({
   waterVolume: Joi.number().integer().min(1).max(5000),
   date: Joi.date(),
 });
+
+export const waterForMonthSchema = Joi.object({
+  month: Joi.number().min(1).max(12).required().messages({
+    'number.base': 'month should be number',
+    'number.min': 'month should be greater than or equal to 1',
+    'number.max': 'month should be less than or equal to 12',
+    'any.required': 'Month is required',
+  }),
+});
