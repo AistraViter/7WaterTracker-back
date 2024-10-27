0d3dc579b9b7410fc4767339b5e5ad7d63998216
AistraViter <153955169+AistraViter@users.noreply.github.com>
2024-10-27 19:47:12 +0200

    Збільшено термін дії токена

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,10 +9,10 @@ export const CLOUDINARY = {
 };
 
 export const JWT_SECRET = process.env.JWT_SECRET || 'secretKey';
-export const JWT_EXPIRES_IN = '15m';
+export const JWT_EXPIRES_IN = '120m';
 export const REFRESH_TOKEN_EXPIRES_IN = '30d';
 
-export const accessTokenValidUntil = new Date(Date.now() + 15 * 60 * 1000); // Токен на 15 минут
+export const accessTokenValidUntil = new Date(Date.now() + 120 * 60 * 1000); // Токен на 15 минут
 export const refreshTokenValidUntil = new Date(
   Date.now() + 30 * 24 * 60 * 60 * 1000,
 ); // Токен на 30 дней
